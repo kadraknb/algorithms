a42bcfce984da33a5270131f672d1efe0d92d163
kadraknb <kadrak02@hotmail.com>
2023-05-19 09:57:11 -0300

    Criptografia de inversões (Testes)

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,12 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+
+    assert encrypt_message("12345", 2) == "543_21"
+    assert encrypt_message("teste", 3) == "321_54"
+    assert encrypt_message("teste", 6) == "54321"
+
+    with pytest.raises(TypeError):
+        encrypt_message("12345", "string")
