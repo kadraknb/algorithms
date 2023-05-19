00438a4504a2d8c413747be2532d3af1d3b94131
kadraknb <kadrak02@hotmail.com>
2023-05-19 12:47:24 -0300

    Encontrando números repetidos (Algoritmo de busca)

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,17 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+
+    if len(nums) < 2 or type(nums) != list:
+        return False
+
+    counter = dict()
+    for num in nums:
+        if not type(num) == int or num < 0:
+            return False
+        if num not in counter:
+            counter[num] = 1
+        else:
+            return num
+    return False
+
+
+# print(find_duplicate([1, 3, 4, 2, 2]))
